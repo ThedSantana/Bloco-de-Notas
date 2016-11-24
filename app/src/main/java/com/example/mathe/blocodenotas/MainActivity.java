6dbdbb360f3a2bf8f1abcf231594e6f8eedb0a65
Matheus Castro e Silva <matheuscastro824@gmail.com>
2016-11-24 10:11:53 -0200

    Primeira versão do aplicativo

---
+++ app/src/main/java/com/example/mathe/blocodenotas/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.mathe.blocodenotas;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText ettext;
+    Button btcriar;
+    ListView listView;
+
+    private ArrayList<String> notas = new ArrayList<String>();
+    ArrayAdapter<String> adapter = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ettext = (EditText) findViewById(R.id.etttext);
+        btcriar = (Button) findViewById(R.id.btcriar);
+        listView = (ListView) findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,notas);
+        listView.setAdapter(adapter);
+
+        btcriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoNota = ettext.getText().toString();
+                if (textoNota.length() > 0) {
+                    ettext.setText("");
+                    ettext.findFocus();
+                    notas.add(textoNota);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                adb.setTitle("Nota");
+                adb.setMessage("Você deseja apagar essa nota?");
+                final int positionRemove = position;
+                adb.setNegativeButton("Não", null);
+                adb.setPositiveButton("Sim", new AlertDialog.OnClickListener(){
+                    public void onClick(DialogInterface dialogInterface, int which){
+                        notas.remove(positionRemove);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                adb.show();
+            }
+        });
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
